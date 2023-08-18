32b9de1b0cfc08d09d1d371d5d3c8c0021968ead
ssebadduka <seba.jamir@gmail.com>
2023-08-18 00:09:21 -0700

    changed max digits

--- pis_supplier/models.py
+++ pis_supplier/models.py
@@ -34,9 +34,9 @@ class SupplierStatement(models.Model):
         null=True, blank=True,on_delete=models.CASCADE
                                  )
     supplier_amount = models.DecimalField(
-        max_digits=100, decimal_places=2, null=True, blank=True, default=0)
+        max_digits=30, decimal_places=2, null=True, blank=True, default=0)
     payment_amount = models.DecimalField(
-        max_digits=100, decimal_places=2, null=True, blank=True, default=0)
+        max_digits=30, decimal_places=2, null=True, blank=True, default=0)
     description = models.TextField(max_length=500, null=True, blank=True)
     date = models.DateField(null=True, blank=True)
 
